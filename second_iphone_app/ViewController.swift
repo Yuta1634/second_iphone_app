df9c51265fc7ae487725d4c3bc03a958114017af
Yuta1634 <yuta1994year@gmail.com>
2020-09-01 19:17:25 +0900

    Initial commit

---
+++ second_iphone_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  second_iphone_app
+//
+//  Created by 高橋祐太 on 2020/09/01.
+//  Copyright © 2020 高橋祐太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
